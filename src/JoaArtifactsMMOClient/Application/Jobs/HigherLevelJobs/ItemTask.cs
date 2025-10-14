98f817f120b219c2e72335d08584f86bc519c673
Joakim Ousager Andersen <joo@dixa.com>
2025-10-14 08:52:58 +0200

    More fixes for item task

--- src/JoaArtifactsMMOClient/Application/Jobs/HigherLevelJobs/ItemTask.cs
+++ src/JoaArtifactsMMOClient/Application/Jobs/HigherLevelJobs/ItemTask.cs
@@ -61,7 +61,7 @@ public class ItemTask : CharacterJob
         };
     }
 
-    protected override Task<OneOf<AppError, None>> ExecuteAsync()
+    protected override async Task<OneOf<AppError, None>> ExecuteAsync()
     {
         logger.LogInformation($"{JobName}: [{Character.Schema.Name}] run started");
 
@@ -81,17 +81,15 @@ public class ItemTask : CharacterJob
                 ]
             );
             Status = JobStatus.Suspend;
-            return Task.FromResult<OneOf<AppError, None>>(new None());
+            return new None();
         }
 
         // For gather tasks, we are only going to get tasks that we have high enough skill to do, which means that it shouldn't be needed
         // to check if we have enough skill etc. to complete the task
         if (Character.Schema.TaskType != TaskType.items.ToString())
         {
-            return Task.FromResult<OneOf<AppError, None>>(
-                new AppError(
-                    $"Cannot do a {JobName}, because the current task is {Character.Schema.TaskType}"
-                )
+            return new AppError(
+                $"Cannot do a {JobName}, because the current task is {Character.Schema.TaskType}"
             );
         }
 
@@ -108,37 +106,68 @@ public class ItemTask : CharacterJob
             // We queue the job to gather and deposit it before the current job, so we essentially loop this until we are done
             // For now, DepositUnneededItems should also take into account to give things to the tasks master, as a fallback.
 
+            int amountInInventory = Character.GetItemFromInventory(Code)?.Quantity ?? 0;
+
             int amountToObtain = Math.Min(
                 Character.GetInventorySpaceLeft() - 10,
-                amount - progressAmount
+                remainingToGather
             );
 
-            logger.LogInformation(
-                $"{JobName}: [{Character.Schema.Name}]: Need to gather {progressAmount} of {amount} x {Code} - gathering and trading {amountToObtain}, then retriggering this job"
-            );
-
-            var job = new ObtainItem(Character, gameState, Character.Schema.Task, amountToObtain);
-
-            job.CanTriggerTraining = CanTriggerTraining;
-            job.AllowUsingMaterialsFromBank = true;
-
-            job.onSuccessEndHook = async () =>
+            if (amountInInventory >= amountToObtain)
             {
-                int amountInInventory = Character.GetItemFromInventory(itemCode)?.Quantity ?? 0;
-
                 logger.LogInformation(
-                    $"{JobName}: [{Character.Schema.Name}]: onSuccessEndHook for obtain item - trading {amountInInventory} (skip if 0)"
+                    $"{JobName}: [{Character.Schema.Name}]: Found {amountInInventory} x {Code} in inventory - trading in those"
                 );
+                await Character.TaskTrade(itemCode, amountInInventory);
+
+                amountToObtain = 0;
+            }
+            else
+            {
+                amountToObtain -= amountInInventory;
 
                 if (amountInInventory > 0)
                 {
-                    await Character.TaskTrade(itemCode, amountInInventory);
+                    logger.LogInformation(
+                        $"{JobName}: [{Character.Schema.Name}]: Found {amountInInventory} x {Code} in inventory - subtracting those from amount to obtain"
+                    );
                 }
-            };
+            }
 
-            Character.QueueJobsBefore(Id, [job]);
-            Status = JobStatus.Suspend;
-            return Task.FromResult<OneOf<AppError, None>>(new None());
+            if (amountToObtain > 0)
+            {
+                logger.LogInformation(
+                    $"{JobName}: [{Character.Schema.Name}]: Need to gather {progressAmount} of {amount} x {Code} - gathering and trading {amountToObtain}, then retriggering this job"
+                );
+
+                var job = new ObtainItem(
+                    Character,
+                    gameState,
+                    Character.Schema.Task,
+                    amountToObtain
+                );
+
+                job.CanTriggerTraining = CanTriggerTraining;
+                job.AllowUsingMaterialsFromBank = true;
+
+                job.onSuccessEndHook = async () =>
+                {
+                    int amountInInventory = Character.GetItemFromInventory(itemCode)?.Quantity ?? 0;
+
+                    logger.LogInformation(
+                        $"{JobName}: [{Character.Schema.Name}]: onSuccessEndHook for obtain item - trading {amountInInventory} (skip if 0)"
+                    );
+
+                    if (amountInInventory > 0)
+                    {
+                        await Character.TaskTrade(itemCode, amountInInventory);
+                    }
+                };
+
+                Character.QueueJobsBefore(Id, [job]);
+                Status = JobStatus.Suspend;
+                return new None();
+            }
         }
 
         var completeTask = new CompleteTask(
@@ -161,6 +190,6 @@ public class ItemTask : CharacterJob
             $"{JobName}: [{Character.Schema.Name}] - found {jobs.Count} jobs to run, to complete task {Code}"
         );
 
-        return Task.FromResult<OneOf<AppError, None>>(new None());
+        return new None();
     }
 }
